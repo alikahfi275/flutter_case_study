2981d94e27defd1f896177e9e03c4c1dbddc09f9
ali.lubis <ali.lubis@quadrant-si.id>
2025-10-27 17:48:00 +0700

    override sdk and java

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,25 +7,22 @@ plugins {
 
 android {
     namespace = "com.example.case_study"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36 
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_18
+        targetCompatibility = JavaVersion.VERSION_18
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "18"
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.case_study"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 23
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
